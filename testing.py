a5d7767e1055f4c8e48121eb4441a488f5271855
Cristian Campos Agüero <ccampos04@gmail.com>
2023-06-01 12:36:03 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,11 +1,8 @@
-import Examen4;
+import Laboratorio;
 import pytest;
 
 
-def test_proEscalar1():
-  assert examen4.productoEscalar([2, 5, 6], [10, 20, 30]) == 300
-
-parqueo = examen4.Parqueo('San José', 'El parqueo', 2)
+parqueo = Laboratorio.Parqueo('San José', 'El parqueo', 2)
        
 def test_parqueo1():
   assert isinstance(str(parqueo.datosParqueo()), str) == isinstance('San José, 88556699, Capacidad 2', str)
